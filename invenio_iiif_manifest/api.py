97285b72d782b2c0cbc1f31a23679e9e1207460d
Hayashi Yohei <yohei@nii.ac.jp>
2018-09-03 17:48:06 +0900

    invenio-iiif-manifest can generate iiif manifest automaticaly

--- invenio_iiif_manifest/api.py
+++ invenio_iiif_manifest/api.py
@@ -2,13 +2,15 @@ from .models import pid2iiif_ideitifier
 from .iiif_manifest import invenioIIIFManifest
 
 
-
 def generate_iiif_manifest(pid, record):
 	'''generate iiif manifest from image files on the target record from pid'''
 
-	print(dir(pid))
-    #identifiers = pid2iiif_ideitifier(pid)
-	identifiers = ['c22878fe-de33-41cf-917a-418307c26f79:6780eb7f-ec36-4369-974c-37797c011a4c:mars_map_sample.png']
+	identifiers = []
+	files = record.dumps()['_files']
+	for file in files:
+		identifier = ':'.join([file['bucket'], file['version_id'], file['key']])
+		identifiers.append(identifier)
+	#identifiers = ['f104e734-1934-439f-9750-f9770451d0ca:14890973-6d34-4721-8b0a-fa304ebc5f13:mars_map_sample.png']
 	manifest = invenioIIIFManifest('PID: '+pid.pid_value)
 	manifest.add_metadata('pid',str(pid),'en')
 	manifest.set_description('This collection is automatically genarated from Invenio.')
